ded9b5b2baa242e395841d287373aa6e306640dc
stramus <stramus999@gmail.com>
2018-11-07 21:05:33 +0100

    Start architecture

--- src/main/java/stramus/gravitationsuite/proxy/ClientProxy.java
+++ src/main/java/stramus/gravitationsuite/proxy/ClientProxy.java
@@ -1,23 +1,4 @@
 package stramus.gravitationsuite.proxy;
 
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
-public class ClientProxy extends CommonProxy {
-
-    @Override
-    public void preInit(FMLPreInitializationEvent event) {
-
-    }
-
-    @Override
-    public void init(FMLInitializationEvent event) {
-
-    }
-
-    @Override
-    public void postInit(FMLPostInitializationEvent event) {
-
-    }
+public class ClientProxy extends CommonProxy{
 }
---
+++ src/main/java/stramus/gravitationsuite/util/Reference.java
@@ -0,0 +1,12 @@
+package stramus.gravitationsuite.util;
+
+public class Reference {
+
+    public static final String MODID = "gs";
+    public static final String NAME = "Gravitation Suite";
+    public static final String VERSION = "1.0";
+    public static final String CLIENT = "stramus.gravitationsuite.proxy.ClientProxy";
+    public static final String COMMON = "stramus.gravitationsuite.proxy.CommonProxy";
+
+
+}
